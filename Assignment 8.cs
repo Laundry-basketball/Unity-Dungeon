7452ffe6db59517f0c32c64b0dc1689184e17f84
Laundry-basketball <145127654+Laundry-basketball@users.noreply.github.com>
2025-02-24 10:40:24 -0500

    Assignment 8.cs

--- Assignment 8.cs
+++ Assignment 8.cs
@@ -1,6 +1,47 @@
 using UnityEngine;
 
-public class Assignment8
+public class DungeonRoom : MonoBehaviour
 {
-    
+    public GameObject[] Doors { get; set; }
+
+    void Start()
+    {
+        UpdateDoors();
+    }
+
+    void Update()
+    {
+        if (Core.Instance != null)
+        {
+            UpdateDoors();
+        }
+    }
+
+    void UpdateDoors()
+    {
+        for (int i = 0; i < Doors.Length; i++)
+        {
+            Doors[i].SetActive(Core.Instance.AreDoorsVisible);
+        }
+    }
 }
+
+public class Core : MonoBehaviour
+{
+    public static Core Instance;
+    public bool AreDoorsVisible = true;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
